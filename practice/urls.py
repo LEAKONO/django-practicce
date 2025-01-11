0c6a78c1c0758f28b88801b992a4e34bdc235e3c
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2025-01-11 17:50:17 +0300

    added another url

--- practice/urls.py
+++ practice/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.members, name='members'),  
+    path("", views.members, name='members'),
+    path("base/",views.base,name='base')  
 ]
--- practice/views.py
+++ practice/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def members(request):
    return render(request,"index.html")
+def base(request):
+   return render(request,'base.html')
